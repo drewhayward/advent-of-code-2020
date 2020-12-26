fb531df1251bb046d201e2f4b3ac62f7274bbbd4
Drew Hayward <d.andrew.hayward@gmail.com>
2020-12-25 21:59:41 -0500

    switch to linked list solution

--- day23/cups.py
+++ day23/cups.py
@@ -1,73 +1,99 @@
 from tqdm import trange
 
-def find(xs, x):
-    try:
-        return xs.index(x)
-    except ValueError:
-        return -1
+class Node:
+    def __init__(self, value):
+        self.next = None
+        self.prev = None
+        self.value = value
+
+class CircularLinkedList:
+    def __init__(self, items):
+        self.node_map = {}
+        self.current_node = Node(items[0])
+        self.node_map[self.current_node.value] = self.current_node
+        self.max_value = max(items)
+        past = self.current_node
+        for item in items[1:]:
+            node = Node(item)
+            self.node_map[item] = node
+            past.next = node
+            node.prev = past
+            past = node
+        
+        past.next = self.current_node
+        self.current_node.prev = past
+    
+    def __str__(self):
+        s = ''
+        s += f'({str(self.current_node.value)})'
+        pos = self.current_node.next
+        while pos != self.current_node:
+            s += f' {pos.value}'
+            pos = pos.next
+
+        return s
+
+    def find(self, value):
+        return self.node_map[value]
 
 
-def move(cups, current_cup):
-    #print(f"cups: {' '.join([f'({cup})' if cup == current_cup else str(cup) for cup in cups])}")
+    def move(self):
+        # Trim the group out
+        group_start = self.current_node.next
+        group_start.prev = None
+        group_end = group_start.next.next
+        group = [group_start, group_start.next, group_start.next.next]
 
-    max_value = max(cups)
-    list_len =len(cups)
-    curr_idx = cups.index(current_cup)
-    group = cups[curr_idx + 1:curr_idx + 4]
-    if len(group) < 3:
-        group.extend(cups[:(curr_idx + 3) % 4])
+        self.current_node.next = group_end.next
+        group_end.next.prev = self.current_node
 
-    cups = cups[:curr_idx+1] + cups[curr_idx + 4:]
-    if len(cups) > (list_len - 3):
-        cups = cups[(curr_idx + 3) % 4:]
+        group_end.next = None
 
-    #print(f"pick up: {', '.join([str(g) for g in group])}")
+        # Look for the target
+        target_value = ((self.current_node.value - 2) % self.max_value) + 1
+        target = self.find(target_value)
+        while target in group:
+            target_value = ((target_value - 2) % self.max_value) + 1
+            target = self.find(target_value)
 
-    target = (((current_cup - 1) - 1) % max_value) + 1
-    idx = find(cups, target)
-    while idx == -1:
-        target = (((target - 1) - 1) % max_value) + 1
-        idx = find(cups, target)
 
-    #print(f"destination: {target}")
+        # Splice in the group
+        target_end = target.next
+        group_start.prev = target
+        target.next = group_start
+        group_end.next = target_end
+        target_end.prev = group_end
 
-    cups = cups[:idx+1] + group + cups[idx+1:]
-
-    curr_idx = (cups.index(current_cup) + 1) % len(cups)
-    current_cup = cups[curr_idx]
-    return cups, current_cup
+        self.current_node = self.current_node.next
 
 def part_1():
-    cups = [int(char) for char in '586439172']
-    current_cup = cups[0]
+    clist = CircularLinkedList([5, 8, 6, 4, 3, 9, 1, 7, 2])
 
-    for i in range(100):
-        #print(f'-- move {i+1} --')
-        cups, current_cup = move(cups, current_cup)
-        #print()
+    for _ in range(100):
+        clist.move()
 
-    start = cups.index(1) + 1
-    return ''.join(str(cup) for cup in cups[start:] + cups[:start])
+    one_node = clist.find(1)
+    s = ''
+    current_node = one_node.next
+    while current_node != one_node:
+        s += str(current_node.value)
+        current_node = current_node.next
+
+    return s
 
 def part_2():
-    cups = [int(char) for char in '586439172']
+    #nums = [3,8,9,1,2,5,4,6,7]
+    nums = [5, 8, 6, 4, 3, 9, 1, 7, 2]
+    nums.extend(range(10, 1000001))
+    clist = CircularLinkedList(nums)
 
-    # Extend cups
-    max_cup = max(cups)
-    cups.extend(range(max_cup + 1, 1000001))
+    for _ in trange(10000000):
+        clist.move()
 
-    current_cup = cups[0]
-
-    for i in trange(1000000):
-    
-        #print(f'-- move {i+1} --')
-        cups, current_cup = move(cups, current_cup)
-        #print()
-
-    
-    inx = cups.index(current_cup)
-    return cups[idx + 1] * cups[idx + 2]
-
+    one_node = clist.node_map[1]
+    return one_node.next.value * one_node.next.next.value
+        
 if __name__ == "__main__":
     print(part_1())
     print(part_2())
+
